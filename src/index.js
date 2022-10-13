e91a337e00822ed6382ace414eefc6d02dfc065e
Moiwa <38554511+samuelmoiwa@users.noreply.github.com>
2022-10-13 00:21:17 +0000

    add index.js

---
+++ src/index.js
@@ -0,0 +1,55 @@
+import './index.css';
+
+const inputName = document.getElementById('name');
+const inputScore = document.getElementById('score');
+
+const addBtn = document.querySelector('.add_btn');
+const refreshBtn = document.querySelector('.refresh_btn')
+
+let taskList = [];
+
+addBtn.addEventListener('click', () => {
+  if (inputName.value.trim() != 0 && inputScore.value.trim() != 0) {
+    const localItems = JSON.parse(localStorage.getItem('localItem'));
+    if (localItems === null) {
+      taskList = [];
+    } else {
+      taskList = localItems;
+    }
+    taskList.push(`${inputName.value} : ${inputScore.value}`);
+    localStorage.setItem('localItem', JSON.stringify(taskList));
+    inputScore.value = '';
+    inputName.value = '';
+    ClassShowListItems.showItem();
+  }
+});
+
+class ClassShowListItems {
+  static showItem() {
+    const localItems = JSON.parse(localStorage.getItem('localItem'));
+    if (localItems === null) {
+      taskList = [];
+    } else {
+      taskList = localItems;
+    }
+
+    let html = '';
+    const itemShow = document.querySelector('.scoreLists');
+    taskList.forEach((scoreItemList) => {
+      html += `
+    <div class="scoreListItems">
+      <div class="scoreList">
+        <p class="inputText">${scoreItemList}</p>
+      </div>
+    </div>
+    `;
+    });
+    itemShow.innerHTML = html;
+  }
+}
+ClassShowListItems.showItem();
+
+refreshBtn.addEventListener('click', () => {
+    window.location.reload()
+})
+
