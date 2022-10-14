2de3ad9f6b3fe923bae9f4d344298ced4cc77af3
Moiwa <38554511+samuelmoiwa@users.noreply.github.com>
2022-10-14 20:21:29 +0000

    add displayLeader.js

---
+++ src/modules/displayLeader.js
@@ -0,0 +1,19 @@
+const newLeader = document.querySelector('.table_item');
+const displayLeader = (data) => {
+  while (newLeader.hasChildNodes()) {
+    newLeader.removeChild(newLeader.lastChild);
+  }
+  data.result.sort((a, b) => a.score - b.score);
+
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < data.result.length; i++) {
+    const newRow = document.createElement('tr');
+    newRow.innerHTML = `
+        <td>${data.result[i].user}</td>
+        <td>: ${data.result[i].score}</td>
+    `;
+    newLeader.appendChild(newRow);
+  }
+};
+
+export default displayLeader;
